5bf3497e3ff5bb1dc83dabd57cedfd6aae0a3cc5
Paul McConnell <pmc.consult@gmail.com>
2017-02-14 08:16:01 -0600

    add unit test coverage to BoggleModel

--- BoggleGameTests/BoggleGameTests.swift
+++ BoggleGameTests/BoggleGameTests.swift
@@ -3,14 +3,32 @@ import XCTest
 
 class BoggleGameTests: XCTestCase {
     
+    var model: BoggleModel!
+    
     override func setUp() {
         super.setUp()
+        model = BoggleModel()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func test_AddLetterToCurrentWord() {
+        model.addLetterToCurrentWord(letter: "h")
+        model.addLetterToCurrentWord(letter: "i")
+        
+        XCTAssertEqual("hi", model.currentWord)
+    }
+    
+    func test_SubmitWordClearsCurrentWord() {
+        model.addLetterToCurrentWord(letter: "h")
+        model.addLetterToCurrentWord(letter: "i")
+        model.addWord()
+
+        XCTAssertEqual("", model.currentWord)
     }
     
     func testExample() {
